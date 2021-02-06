eb45fa8b48fc9f464ecbca224b9fe76a7dc2465e
Michael Lohmann <michael.lohmann@bevuta.com>
2021-02-06 12:53:28 +0100

    Revert "nixos-version: redirect error to stderr"
    This reverts commit e8a72d371003124346896d7bc28a7bcfe081c919.
    This is no longer needed, since this pull request does not depend on this
    anymore

--- nixos/modules/installer/tools/nixos-version.sh
+++ nixos/modules/installer/tools/nixos-version.sh
@@ -7,7 +7,7 @@ case "$1" in
     ;;
   --hash|--revision)
     if ! [[ @revision@ =~ ^[0-9a-f]+$ ]]; then
-      echo "$0: Nixpkgs commit hash is unknown" >&2
+      echo "$0: Nixpkgs commit hash is unknown"
       exit 1
     fi
     echo "@revision@"
